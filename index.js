bf4cc9849817af27aecebeff637b8ab18121697e
YusungAN <52990109+YusungAN@users.noreply.github.com>
2020-11-15 17:13:50 +0900

    반응형

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let is4 = false;
+
 AOS.init();
 
 new fullpage("#fullpage", {
@@ -5,3 +7,28 @@ new fullpage("#fullpage", {
         $(".fp-table.active .aos-init").addClass("aos-animate");
     },
 });
+
+function scroolEvt() {
+    if (!is4) {
+        let slideNumber = $(".fp-section.active").index() + 1;
+        if (slideNumber === 5) {
+            is4 = !is4;
+            const lline = document.getElementById("left-line");
+            const rline = document.getElementById("right-line");
+            let len = 0;
+            let repeat;
+            repeat = setInterval(() => {
+                lline.style.width = `${len}%`;
+                rline.style.width = `${len}%`;
+                rline.style.marginLeft = `${100 - len}%`;
+                len++;
+                if (len === 75) {
+                    is4 = true;
+                    clearInterval(repeat);
+                }
+            }, 15);
+        }
+    }
+}
+
+window.addEventListener("mousewheel", scroolEvt);
